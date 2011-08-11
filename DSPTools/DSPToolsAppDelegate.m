6593692cd7bd46a27bf7667407d3318ef98607f3
Puru <puruchoudhary@gmail.com>
2011-08-11 14:30:52 -0700

    Added Three20 to the project

--- DSPTools/DSPToolsAppDelegate.m
+++ DSPTools/DSPToolsAppDelegate.m
@@ -3,7 +3,7 @@
 //  DSPTools
 //
 //  Created by Puru Choudhary on 8/10/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Puru Choudhary. All rights reserved.
 //
 
 #import "DSPToolsAppDelegate.h"
